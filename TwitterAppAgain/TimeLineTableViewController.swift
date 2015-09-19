0ceb8f8d46a837f8d92274e9c8953a6533bb8dac
Yoshihisa Haruyama <uclaaerospace@gmail.com>
2015-09-19 18:30:09 +0900

    Tweet画面がでるようにした。

--- TwitterAppAgain/TimeLineTableViewController.swift
+++ TwitterAppAgain/TimeLineTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class TimeLineTableViewController: UITableViewController {
 
@@ -49,6 +50,38 @@ class TimeLineTableViewController: UITableViewController {
     
     }
 
+    @IBAction func tapTweetButton(sender: UIBarButtonItem) {
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            
+            let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            
+            //CancelもしくはPostを押した際に呼ばれ、投稿画面を閉じる処理を行っています。
+            vc.completionHandler = {(result:SLComposeViewControllerResult) -> () in
+                vc.dismissViewControllerAnimated(true, completion:nil)
+            }
+            
+            ////投稿画面の初期値設定
+            //vc.setInitialText("初期テキストを設定できます。")
+            //vc.addURL(NSURL(string:"シェアURLを設定できます。"))
+            self.presentViewController(vc, animated: true, completion: nil)
+            
+        }else{
+            let alert = UIAlertController(title: "エラー", message: "Twitterアカウントが登録されていません。設定アプリを開きますか？", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "はい", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                if let URL = NSURL(string: UIApplicationOpenSettingsURLString){
+                    UIApplication.sharedApplication().openURL(URL)
+                }
+            }))
+            
+            alert.addAction(UIAlertAction(title: "いいえ", style: UIAlertActionStyle.Default, handler:nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+        
+        
+    }
     
     
 }
